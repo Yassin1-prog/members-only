8e0aa8318eaf383231f04b2505ccfeafb44c62e1
Yassin1-prog <132544868+Yassin1-prog@users.noreply.github.com>
2024-10-18 20:22:54 +0300

    render deply

--- database/populatedb.js
+++ database/populatedb.js
@@ -24,6 +24,9 @@ async function main() {
   console.log("seeding...");
   const client = new Client({
     connectionString: process.argv[2],
+    ssl: {
+      rejectUnauthorized: false, // Add SSL
+    },
   });
   await client.connect();
   await client.query(SQL);
