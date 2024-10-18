425983bcfbd5305c802577414f5cd2772fad7cf4
Yassin1-prog <132544868+Yassin1-prog@users.noreply.github.com>
2024-10-18 16:42:34 +0300

    deployment setup

--- db.js
+++ db.js
@@ -1,10 +1,14 @@
 const { Pool } = require("pg");
+require("dotenv").config();
 
-const connectionString = "postgresql://yassin:hamza@localhost:5432/members";
+const connectionString =
+  process.env.NODE_ENV === "production"
+    ? process.env.DATABASE_URL // use Railway's DATABASE_URL in production
+    : process.env.LOCAL_DATABASE_URL; // use local DB for development
 
 module.exports = new Pool({
   connectionString: connectionString,
   ssl: {
-    rejectUnauthorized: false,
+    rejectUnauthorized: false, // Add SSL
   },
 });
